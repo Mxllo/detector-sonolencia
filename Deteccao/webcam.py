ad48a61c5250b5f2d21ee6f80558aff4d53004b0
Mxllo <lucasmellog@gmail.com>
2022-04-28 21:33:24 -0300

    Detecção de boca com alarme

--- Deteccao/webcam.py
+++ Deteccao/webcam.py
@@ -6,11 +6,12 @@ classificador = cv.CascadeClassifier(cv.data.haarcascades + 'haarcascade_frontal
 classificadorOlhos = cv.CascadeClassifier(cv.data.haarcascades + 'haarcascade_eye.xml')
 classificadorBoca = cv.CascadeClassifier(cv.data.haarcascades + 'haarcascade_mcs_mouth.xml')
 
-countOlhos = 0
-countBoca = 0
+isOlhosDetectados = False
+isBocaDetectada = False
+isFaceDetectada = False
 
 font                   = cv.FONT_HERSHEY_SIMPLEX
-bottomLeftCornerOfText = (10,500)
+bottomLeftCornerOfText = (50,50)
 fontScale              = 1
 fontColor              = (255,255,255)
 thickness              = 1
@@ -18,43 +19,58 @@ lineType               = 2
 
 video = cv.VideoCapture(0)
 
+def detectaSonolencia(isOlhosDetectados, isBocaDetectada, isFaceDetectada):
+    if(isOlhosDetectados == True and isBocaDetectada == True and isFaceDetectada == True):
+        cv.putText(frame, "Não detectado", bottomLeftCornerOfText, font, fontScale, fontColor, thickness, lineType)
+    else:
+        print("detectou sonolencia")
+        print("Olhos: ", isOlhosDetectados)
+        print("Boca: ", isBocaDetectada)
+        print("Face: ", isFaceDetectada)
+        cv.putText(frame, "Sonolencia detectada", bottomLeftCornerOfText, font, fontScale, fontColor, thickness, lineType)
+        
+
 while True:
     conectado, frame = video.read()
     frame = cv.flip(frame, 1)
     frameCinza = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
     facesDetectadas = classificador.detectMultiScale(frameCinza, scaleFactor=1.08, minNeighbors=8, minSize=(30, 30))
     for (x, y, l, a) in facesDetectadas:
+        if(len(facesDetectadas) > 0):
+            isFaceDetectada = True
+        else:
+            isFaceDetectada = False
+
         cv.rectangle(frame, (x, y), (x + l, y + a), (0, 0, 255), 2)
+
         regiao = frame[y:y + a, x:x + l]
         regiaoCinzaOlho = cv.cvtColor(regiao, cv.COLOR_BGR2GRAY)
         olhosDetectados = classificadorOlhos.detectMultiScale(regiaoCinzaOlho, scaleFactor=1.08, minNeighbors=9, minSize=(30, 30))
+        print(len(olhosDetectados))
         for (ox, oy, ol, oa) in olhosDetectados:
-            cv.rectangle(regiao, (ox, oy), (ox + ol, oy + oa), (0, 255, 0), 2)
-            if(len(olhosDetectados) < 2):
-                countOlhos += 1
-                if(countOlhos > 12):
-                    cv.putText(frame, 
-                'ALERTA! SONOLENCIA DETECTADA', 
-                (50, 50), 
-                font, 1, 
-                (0, 255, 255), 
-                2, 
-                cv.LINE_4)
-                    print("Olho não detectado")  #aviso de sonolencia
+            if(len(olhosDetectados) >= 2):
+                isOlhosDetectados = True
             else:
-                countOlhos = 0
+                isOlhosDetectados = False
+
+            cv.rectangle(regiao, (ox, oy), (ox + ol, oy + oa), (0, 255, 0), 2)
+
         regiaoCinzaBoca = cv.cvtColor(regiao, cv.COLOR_BGR2GRAY)
         bocaDetectada = classificadorBoca.detectMultiScale(regiaoCinzaBoca, minSize=(150, 20))
+        
         for (bx, by, bl, ba) in bocaDetectada:
-            cv.rectangle(regiao, (bx, by), (bx + bl, by + ba), (0, 255, 0), 2)
-            if(len(bocaDetectada) < 1):
-                countBoca += 1
-                if(countBoca > 5):
-                    print("Boca não detectada")  #aviso de sonolencia
+            if(len(bocaDetectada) >= 1):
+                isBocaDetectada = True
             else:
-                countBoca = 0        
+                isBocaDetectada = False
 
-            
+            cv.rectangle(regiao, (bx, by), (bx + bl, by + ba), (0, 255, 0), 2)   
+        
+    
+    detectaSonolencia(isOlhosDetectados, isBocaDetectada, isFaceDetectada)
+        
     cv.imshow('Video', frame)
     if cv.waitKey(1) & 0xFF == ord('q'):
-        break          
+        break   
+
+
